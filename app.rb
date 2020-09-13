d5b7a428b8e5bf4f2bc3aec9f0b09ad509280f95
Micah Bowie <micahbowie20@gmail.com>
2020-09-13 18:34:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
  
+  get '/name' do
+    "Hello, World!"
+  end
+  
+  get '/hometown' do
+    "Hello, World!"
+  end
+  
+  get '/favorite-song' do
+    "Hello, World!"
+  end
+  
   get '/' do
     "Hello, World!"
   end
