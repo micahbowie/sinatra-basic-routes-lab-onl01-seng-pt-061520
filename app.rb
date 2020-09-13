0ab873a198a3ae67c7681cb2b6d91f82dad622ca
Micah Bowie <micahbowie20@gmail.com>
2020-09-13 18:35:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+ 
+  get '/name' do
+     "My name is __" 
+  end
+  
+  get '/hometown' do
+    "My hometown is __"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  
 end
